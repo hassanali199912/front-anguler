2668628b6b07cee6891ff8208d20cda5845df3d2
hassanali199912 <adsststjntun@gmail.com>
2025-05-18 21:35:18 +0300

    :fire: Front End Angler Project With .NET Web Apis

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from "../view/main/main.component"
+import { LoginComponent } from "../view/login/login.component"
+import { RegisterComponent } from "../view/register/register.component"
+import { HomeComponent } from "../feature/home/home.component"
+import { ProductsComponent } from "../feature/products/products.component"
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent,
+        children: [
+            { path: '', component: HomeComponent },
+            { path: 'product', component: ProductsComponent },
+        ]
+    },
+    { path: "login", component: LoginComponent },
+    { path: "register", component: RegisterComponent }
+];
 
-export const routes: Routes = [];
