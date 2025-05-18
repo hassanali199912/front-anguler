40bcc21c85a0796efaef8c1e5706a88fc9f86208
hassanali199912 <adsststjntun@gmail.com>
2025-05-18 21:54:20 +0300

    route the pages

--- src/view/main/main.component.ts
+++ src/view/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
-import { RouterOutlet} from "@angular/router"
+import { RouterOutlet } from "@angular/router"
+import { NavComponent } from "../../components/nav/nav.component"
+import { SideNavComponent } from "../../components/side-nav/side-nav.component"
 @Component({
   selector: 'app-main',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, SideNavComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
